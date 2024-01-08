d91a1f8a416853c9b42b5998dbb0831120beacc4
Karabo Mjiyako <karabomjiyako@icloud.com>
2024-01-08 13:52:33 +0200

    "Committing changes"

---
+++ week-4-homework/week-4-homework/src/App.js
@@ -0,0 +1,14 @@
+import Search from './Search';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+        <Search />
+        <p>This project was coded by <a href="https://github.com/KarabooMjiyako?tab=repositories">Karabo Mjiyako</a> and is <a href="#" >open-sourced</a> </p>
+    </div>
+  );
+}
+
+
